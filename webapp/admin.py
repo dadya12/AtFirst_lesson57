46d0216a6b669a67edde431db63495467fbb0d7a
Adam <adam.nemmish213@gmail.com>
2024-05-17 19:09:34 +0600

    Выполнил 1 этап

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Tag, Status, Type
 
-# Register your models here.
+
+@admin.register(Tag)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description']
+    list_filter = ['status', 'type']
+    search_fields = ['status', 'type',  'id']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Status)
+admin.site.register(Type)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    summary = models.CharField(verbose_name='Краткое описание', max_length=100)
+    description = models.TextField(verbose_name='Описание', max_length=400, blank=False, null=False)
+    status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.CASCADE)
+    type = models.ForeignKey('webapp.Type', related_name='type', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+
+
+class Status(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Type(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
